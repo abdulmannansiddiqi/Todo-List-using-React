15f2f953c566f931c411c1d4e54ca5ce51ce48f0
abdulmannansiddiqi <abdulmannansiddiqi7@gmail.com>
2025-07-15 13:03:30 +0500

    Nabbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
 
 
 function App() {
@@ -8,9 +9,24 @@ function App() {
 
   return (
     <>
-     <div className="container">
-      <div className='bg-amber-800'>hiiiiii</div>
-     </div>
+      <Navbar />
+      <div className="container mx-auto mt-5  rounded-xl p-5    bg-slate-300 min-h-screen ">
+        <div className="addTodo">
+          <h1 className='text-lg font-bold'>Add a Todo</h1>
+          <input type="text" />
+          <button>Submit</button>
+        </div>
+        <h1 className='text-lg font-bold  '>Your Todos</h1>
+        <div className="todos">
+          <div className="todo flex">
+            <div className="text">Lorem ipsum dolor, sit amet consectetur adipisicing.</div>
+            <div className="buttons">
+              <button>Edit</button>
+              <button>Del</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className='flex justify-between bg-slate-700 text-white py-3'>
+      <div className="logo">
+        <span className='font-bold text-xl mx-8'>iTask</span>
+      </div>
+      <ul className='flex gap-9 mx-8 cursor-pointer' >
+        <li className='hover:font-bold cursor-pointer transition-all duration-500'>Home</li>
+        <li className='hover:font-bold cursor-pointer transition-all duration-500'>Your Task</li>
+      </ul>
+    </nav>
+  ) 
 }
 
 export default Navbar
