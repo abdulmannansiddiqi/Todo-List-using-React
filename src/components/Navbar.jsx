ec63a37bd157754ce68a12cfe86b032321cc2b4a
abdulmannansiddiqi <abdulmannansiddiqi7@gmail.com>
2025-07-18 22:34:01 +0500

    addd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,7 @@ const Navbar = () => {
         <li className='hover:font-bold cursor-pointer transition-all duration-100'>Home</li>
         <li className='hover:font-bold cursor-pointer '>Your Task</li>
       </ul>
-        <li className='hover:font-bold cursor-pointer transition-all duration-100'>Home</li>
-        <li className='hover:font-bold cursor-pointer '>Your Task</li>
-      </ul>
+       
         
   ) 
 }
